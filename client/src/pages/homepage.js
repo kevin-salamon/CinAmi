e315b9aaa576a3a4f78a54135c091766582188bd
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-04 14:29:53 -0400

    added handle functions successfully

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
+import { removeMovie, getSavedMovies, updateMovie } from "../utils/API";
 import "../pagestyle.css";
 
 class Homepage extends Component {
@@ -7,6 +8,26 @@ class Homepage extends Component {
         searchTerm: "",
         movieList: [],
     }
+
+    handleGetSavedMovies = () => {
+        getSavedMovies()
+          .then((res) => {
+            this.setState({ movieList: res.data })
+          })
+          .catch(err => console.log(err));
+    }
+
+    handleRemoveMovie = movieId => {
+        removeMovie(movieId)
+          .then(this.handleGetSavedMovies)
+          .catch(err => console.log(err));
+    }
+
+    handleUpdateMovie = (movieId, movieData) => {
+        updateMovie(movieId, movieData)
+          .then(this.handleGetSavedMovies)
+          .catch(err => console.log(err));
+      }
 
     render() {
         return (
