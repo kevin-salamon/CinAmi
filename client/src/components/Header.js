e7ea6f720085735d5c8015a875f08331706e4f56
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-04 14:23:39 -0400

    header => component

---
+++ client/src/components/Header.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+import "../pagestyle.css";
+
+class Header extends Component {
+
+    render() {
+        return (
+            <>
+                <nav className="nav header-custom">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <img src={require("../images/logo.png")} className="logo" alt={"logo"} />
+                            </div>
+                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p className="header-description">One-stop shopping for sharing movie reviews with friends</p>
+                            </div>
+                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}></div>
+                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Search</p>
+                            </div>
+                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Login</p>
+                            </div>
+                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Sign-up</p>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </>
+        );
+    };
+
+}
+
+export default Header;
--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import "./pagestyle.css";
+import Header from "../components/Header";
+import "../pagestyle.css";
 
 class Homepage extends Component {
     state = {
@@ -10,28 +11,7 @@ class Homepage extends Component {
     render() {
         return (
             <>
-                <nav className="nav header-custom">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                                <img src={require("../images/logo.png")} className="logo" alt={"logo"} />
-                            </div>
-                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                                <p className="header-description">One-stop shopping for sharing movie reviews with friends</p>
-                            </div>
-                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}></div>
-                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                                <p>Search</p>
-                            </div>
-                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                                <p>Login</p>
-                            </div>
-                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
-                                <p>Sign-up</p>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
+                <Header />
             </>
         );
     };
