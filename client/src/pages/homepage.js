5ae5599008df6f69df3b115f6a5eba09497b4054
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-04 13:05:44 -0400

    added basic header for templating. added favicon

--- client/src/pages/homepage.js
+++ client/src/pages/homepage.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./pagestyle.css";
 
 class Homepage extends Component {
     state = {
@@ -9,7 +10,28 @@ class Homepage extends Component {
     render() {
         return (
             <>
-                <h1>test</h1>
+                <nav className="nav header-custom">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <img src={require("../images/logo.png")} className="logo" alt={"logo"} />
+                            </div>
+                            <div className="col-lg-3" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p className="header-description">One-stop shopping for sharing movie reviews with friends</p>
+                            </div>
+                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}></div>
+                            <div className="col-lg-2" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Search</p>
+                            </div>
+                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Login</p>
+                            </div>
+                            <div className="col-lg-1" style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+                                <p>Sign-up</p>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
             </>
         );
     };
