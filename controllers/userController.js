14844ae7588976963ec8f82a2aa15a665d1e8e03
kevin-salamon <kevinwebdevs@gmail.com>
2020-06-08 21:41:14 -0400

    adding bcrypt and jwt authentication routes and controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,17 @@
 const db = require("../models");
 const bcrypt = require("bcryptjs");
+const config = require("config");
+const jwt = require("jsonwebtoken");
 
 module.exports = {
+
   getSavedUsers: function(req, res) {
     db.User.find({}).then(dbUserData => res.json(dbUserData)).catch(err => {
       console.log(err);
       res.json(err);
     });
   },
+
   saveUser: function(req, res) {
     const { name, email, password } = req.body;
     if (!name || !email || !password) {
@@ -30,20 +34,30 @@ module.exports = {
         bcrypt.hash(newUser.password, salt, (err, hash) => {
           if (err) throw err;
           newUser.password = hash;
-          newUser.save().then(user => {
-            res.json({user: {
-              id: user.id,
-              name: user.name,
-              email: user.email
-            }})
+          newUser.save()
+          .then(user => {
+
+            jwt.sign(
+              { id: user.id },
+              config.get('jwtSecret'),
+              { expiresIn: 3600 },
+              (err, token) => {
+                if (err) throw err;
+                res.json({
+                  token,
+                  user: {
+                    id: user.id,
+                    name: user.name,
+                    email: user.email
+                  }
+                });
+              }
+            )
           })
         })
       })
     })
-    // db.User.create(req.body).then(dbUserData => res.json(dbUserData)).catch(err => {
-    //   console.log(err);
-    //   res.json(err);
-    // });
   }
+  
 };
 
